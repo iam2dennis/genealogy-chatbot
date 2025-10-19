529bcbd51e05883280e49c09c8334487f62f0677
iam2dennis <iam2dennis@gmail.com>
2025-10-18 21:25:20 -0400

    Add files via upload

--- Icons.tsx
+++ Icons.tsx
@@ -75,6 +75,12 @@ export const CopyIcon: React.FC<IconProps> = ({ className }) => (
     </svg>
 );
 
+export const CheckIcon: React.FC<IconProps> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <polyline points="20 6 9 17 4 12" />
+    </svg>
+);
+
 export const RestartIcon: React.FC<IconProps> = ({ className }) => (
   <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
