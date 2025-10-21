cf94916b41fffc4618f862138f85f0819bfdd0d1
iam2dennis <iam2dennis@gmail.com>
2025-10-21 18:23:44 -0400

    Add files via upload

---
+++ components/geminiService.ts
@@ -0,0 +1,10 @@
+// FIX: Commented out to prevent type resolution error, likely due to environment setup.
+// /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
