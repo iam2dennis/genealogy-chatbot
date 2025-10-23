f965a963101e09387bc2c5043bb0ea29c96e699b
iam2dennis <iam2dennis@gmail.com>
2025-10-23 00:30:48 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    // Expose the API_KEY environment variable to the client-side code.
-    // This is a more direct and robust method.
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
 })
